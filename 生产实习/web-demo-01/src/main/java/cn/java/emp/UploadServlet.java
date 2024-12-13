145c78eac93e96d184faf25825cf978278c370f7
Xuding0829 <2373700847@qq.com>
2024-12-13 15:27:34 +0800

    test

--- 生产实习/web-demo-01/src/main/java/cn/java/emp/UploadServlet.java
+++
@@ -1,9 +0,0 @@
-package cn.java.emp;
-
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-
-@WebServlet("/upload2")
-public class UploadServlet extends HttpServlet {
-
-}
